a9a0e447019d3ea461f80701709ee006f3ab3b9f
W4Jamin <yinkop@hotmail.com>
2019-04-09 21:51:30 -0400

    End chapter 21

--- src/main/java/guru/springframework/services/ImageServiceImpl.java
+++ src/main/java/guru/springframework/services/ImageServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.services;
 
 import guru.springframework.domain.Recipe;
 import guru.springframework.repositories.RecipeRepository;
+import guru.springframework.repositories.reactive.RecipeReactiveRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Mono;
 
 import java.io.IOException;
 
@@ -17,36 +19,38 @@ import java.io.IOException;
 public class ImageServiceImpl implements ImageService {
 
 
-    private final RecipeRepository recipeRepository;
+    private final RecipeReactiveRepository recipeReactiveRepository;
 
-    public ImageServiceImpl( RecipeRepository recipeService) {
+    public ImageServiceImpl( RecipeReactiveRepository recipeReactiveRepository) {
 
-        this.recipeRepository = recipeService;
+        this.recipeReactiveRepository = recipeReactiveRepository;
     }
 
     @Override
     @Transactional
-    public void saveImageFile(String recipeId, MultipartFile file) {
+    public Mono<Object> saveImageFile(String recipeId, MultipartFile file) {
+        Mono<Recipe> recipe = recipeReactiveRepository.findById(recipeId)
+                .map(rc -> {
+                    try {
+                        Byte[] byteObjects = new Byte[file.getBytes().length];
+                        int i = 0;
 
-        try {
-            Recipe recipe = recipeRepository.findById(recipeId).get();
+                        for (byte b : file.getBytes()){
+                            byteObjects[i++] = b;
+                        }
 
-            Byte[] byteObjects = new Byte[file.getBytes().length];
+                        rc.setImage(byteObjects);
+                        return rc;
+                    }
+                    catch (IOException e) {
+                        //todo handle better
+                        log.error("Error occurred", e);
+                        e.printStackTrace();
+                        throw new RuntimeException(e);
+                    }
+                });
+        recipeReactiveRepository.save(recipe.block()).block();
 
-            int i = 0;
-
-            for (byte b : file.getBytes()){
-                byteObjects[i++] = b;
-            }
-
-            recipe.setImage(byteObjects);
-
-            recipeRepository.save(recipe);
-        } catch (IOException e) {
-            //todo handle better
-            log.error("Error occurred", e);
-
-            e.printStackTrace();
-        }
+        return Mono.empty();
     }
 }
---
+++ src/test/java/guru/springframework/repositories/reactive/UnitOfMeasureReactiveRepositoryTest.java
@@ -0,0 +1,46 @@
+package guru.springframework.repositories.reactive;
+
+import guru.springframework.domain.UnitOfMeasure;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+
+@RunWith(SpringRunner.class)
+@DataMongoTest
+public class UnitOfMeasureReactiveRepositoryTest {
+
+    @Autowired
+    UnitOfMeasureReactiveRepository unitOfMeasureReactiveRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        unitOfMeasureReactiveRepository.deleteAll().block();
+    }
+
+    @Test
+    public void saveUnitOfMeasure() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setDescription("Foo");
+
+        unitOfMeasureReactiveRepository.save(unitOfMeasure).block();
+
+        assertEquals(Long.valueOf(1L), unitOfMeasureReactiveRepository.count().block());
+    }
+
+    @Test
+    public void findByDescription() {
+        UnitOfMeasure unitOfMeasure = new UnitOfMeasure();
+        unitOfMeasure.setDescription("Foo");
+
+        unitOfMeasureReactiveRepository.save(unitOfMeasure).block();
+
+        assertNotNull(unitOfMeasureReactiveRepository.findByDescription("Foo"));
+    }
+}
